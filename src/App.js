6394ceea3a4f1260628b7de94aebe6c59f45ad8d
Abhil_K_Vidhyadharan <abhil@keyvalue.systems>
2020-02-08 11:30:43 +0530

    State and Class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class sample extends React.Component {
+  constructor() {
+    super ()
+      this.state = {
+        count: 0,
+        login: false
+      }
+  }
+  handleClick = () => {
+    this.setState((prevState)=> {
+      return {
+        count: prevState.count + 1
+      }
+    })
+  }
+  handleClickM = () => {
+    this.state.count > 0 && this.setState((prevState)=> {
+      return {
+        count: prevState.count - 1
+      }
+    })
+  }
+  handleClickR = () => {
+    this.setState((prevState)=> {
+      return {
+        count: 0
+      }
+    })
+  }
+  handleLogin = () => {
+    this.handleClickR();
+    this.setState((prevState)=> {
+      return {
+        login: !prevState.login
+      }
+    })
+  }
+  render() {
+    return (
+      <div className="main">
+        { this.state.login ? 
+        <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick} className="btn">
+          <h1> + </h1>
+        </button>
+        <button onClick={this.handleClickM} className="btn">
+          <h1> - </h1>
+        </button>
+        <button onClick={this.handleClickR} className="btn">
+          <h1> RESET </h1>
+        </button>
+        <button onClick={this.handleLogin} className="btnL">
+          LOG OUT
+        </button>
+      </div>
+      :
+      <div>
+        <button onClick={this.handleLogin}>
+          LOGIN
+        </button>
+      </div>}
+      </div>
+    )
+  }
 }
-
-export default App;
+export default sample;
